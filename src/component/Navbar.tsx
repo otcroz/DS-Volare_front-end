9ca2e6ae170699b9b94c378f48d242986cf4c2a0
yxxyn20 <dbtndus0527@gmail.com>
2024-06-28 17:21:42 +0900

    [design] Implement a navbar

--- src/component/Navbar.tsx
+++ src/component/Navbar.tsx
@@ -1,13 +1,47 @@
 import React from 'react';
+import styled from 'styled-components';
+import { useNavigate } from 'react-router-dom';
 
-type props = {
-  name: string;
+const NavBar = () => {
+  const navigate = useNavigate();
+
+  const navigateConvertScript = () => {
+    navigate('/convert');
+  };
+
+  return (
+    <Container>
+      <Logo />
+      <Text onClick={navigateConvertScript}>대본 변환</Text>
+      <div style={{ flex: 1 }} />
+      <Text>회원가입</Text>
+      <Text>로그인</Text>
+    </Container>
+  );
 };
 
-const NavBar = ({ name }: props) => (
-  <>
-    <div>Hello, {name}</div>
-  </>
-);
+// text
+const Text = styled.span`
+  font-size: 20px;
+  color: white;
+`;
+
+// container
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  height: 80px;
+  padding: 0 50px;
+  gap: 30px;
+  background-color: ${({ theme }) => theme.colors.baseColor1};
+`;
+
+// component
+const Logo = styled.image`
+  width: 50px;
+  height: 50px;
+  border-radius: 50px;
+  background-color: white;
+`;
 
 export default NavBar;
