d075783ef88ecbfe47c679a050e32ede648493df
otcroz <dbtndus0527@gmail.com>
2024-07-27 20:24:00 +0900

    [chore] Install @tanstack/react-query, initialize queryClient code
    Github issue #37

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,35 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import MainPage from './page/MainPage';
 import MyPage from './page/Mypage';
 import ConvertPage from './page/ConvertPage';
 import NavBar from './component/base/Navbar';
 
+// initialize queryClient
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60,
+    },
+  },
+});
+
 const App = () => {
   return (
     <>
-      <Router>
-        <NavBar />
-        <Routes>
-          <Route path="/main/" element={<MainPage />} />
-          <Route path="/mypage/" element={<MyPage />} />
-          <Route path="/convert/" element={<ConvertPage />} />
-        </Routes>
-      </Router>
+      <QueryClientProvider client={queryClient}>
+        <Router>
+          <NavBar />
+          <Routes>
+            <Route path="/main/" element={<MainPage />} />
+            <Route path="/mypage/" element={<MyPage />} />
+            <Route path="/convert/" element={<ConvertPage />} />
+          </Routes>
+        </Router>
+        <ReactQueryDevtools initialIsOpen={true} />
+      </QueryClientProvider>
     </>
   );
 };
