5aa5832271ecdfc0319b64700ee8ced6a172e143
otcroz <dbtndus0527@gmail.com>
2024-08-15 16:08:42 +0900

    [feat] Connect to API in convert page: cognize the character, convert to script - flask API: cognize the character - spring boot API: convert to script Github issue #45

--- src/hooks/useConvert.ts
+++ src/hooks/useConvert.ts
@@ -4,6 +4,7 @@ import { useUser } from './useUser';
 export const useConvert = () => {
   const { getTokenUser } = useUser();
 
+  // api: save novel / spring
   const saveNovel = async (title: string, novel: string) => {
     const { accessToken } = getTokenUser();
     const headers = {
@@ -20,7 +21,7 @@ export const useConvert = () => {
         headers: headers,
       });
       const data = result.data;
-      if (data.isSuceess) {
+      if (data.isSuccess) {
         console.log(data.message);
         return true;
       } else {
@@ -32,7 +33,61 @@ export const useConvert = () => {
     }
   };
 
+  // api: cognize the character / flask
+  const cognizeCharacter = async (text: string) => {
+    try {
+      const result = await axios.post(
+        `/flask/convert_script/character_cognizing`,
+        { text }
+      );
+      return result.data;
+    } catch (err) {
+      console.log(err); // temporary error handling
+    }
+  };
+
+  // api: convert a script / spring
+  const convertScript = async (
+    candidates: string[],
+    text: string,
+    novelId: string
+  ) => {
+    const { accessToken } = getTokenUser();
+    const headers = {
+      'X-AUTH-TOKEN': accessToken,
+    };
+
+    const requestData = {
+      candidates: candidates,
+      text: text,
+    };
+
+    try {
+      const result = await axios.post(
+        `/spring/scripts/${novelId}`,
+        requestData,
+        {
+          headers: headers,
+        }
+      );
+
+      const data = result.data;
+      console.log(data);
+      if (data.isSuccess) {
+        console.log(data.message);
+        return data.result; // return script
+      } else {
+        console.log(data.message);
+        return false;
+      }
+    } catch (err) {
+      console.log(err); // temporary error handling
+    }
+  };
+
   return {
     saveNovel,
+    cognizeCharacter,
+    convertScript,
   };
 };
