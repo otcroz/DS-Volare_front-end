82175899b328a94f50cf0698cfc313afdd518801
otcroz <dbtndus0527@gmail.com>
2024-08-15 17:42:03 +0900

    [feat] Implement context using convert page - Implement a title, novel, character - declare a script, storyboard, statistics Github issue #47

---
+++ src/context/convertDataContext.tsx
@@ -0,0 +1,92 @@
+import React, { createContext, useContext, useState } from 'react';
+
+// 타입 정의
+type NovelTitleContextType = {
+  title: string;
+  setTitle: React.Dispatch<React.SetStateAction<string>>;
+};
+
+type NovelContextType = {
+  text: string;
+  setText: React.Dispatch<React.SetStateAction<string>>;
+};
+
+type CharacterContextType = {
+  characterList: string[];
+  setCharacterList: React.Dispatch<React.SetStateAction<string[]>>;
+};
+
+type ScriptContextType = {};
+
+type StoryboardContextType = {};
+
+type StatisticsContextType = {};
+
+// 전역: 컨택스트 정의
+const NovelTitleContext = createContext<NovelTitleContextType | undefined>({
+  title: '',
+  setTitle: () => {},
+});
+
+const NovelContext = createContext<NovelContextType | undefined>({
+  text: '',
+  setText: () => {},
+});
+
+const CharacterContext = createContext<CharacterContextType | undefined>({
+  characterList: [],
+  setCharacterList: () => {},
+});
+
+// 컨택스트를 전역적으로 제공할 수 있는 프로바이더 작성, 컴포넌트 순서 상관X
+const ConvertDataProvider = ({ children }: { children: React.ReactNode }) => {
+  const [text, setText] = useState<string>('');
+  const [characterList, setCharacterList] = useState<string[]>([]);
+  const [title, setTitle] = useState<string>('');
+
+  return (
+    <NovelTitleContext.Provider value={{ title, setTitle }}>
+      <NovelContext.Provider value={{ text, setText }}>
+        <CharacterContext.Provider value={{ characterList, setCharacterList }}>
+          {children}
+        </CharacterContext.Provider>
+      </NovelContext.Provider>
+    </NovelTitleContext.Provider>
+  );
+};
+
+// 컨텍스트 값을 사용하는 커스텀 훅
+const useNovelTitleData = () => {
+  const context = useContext(NovelTitleContext);
+  if (context === undefined) {
+    throw new Error(
+      'useNovelTitleData must be used within a ConvertDataProvider'
+    );
+  }
+  return context;
+};
+
+const useNovelData = () => {
+  const context = useContext(NovelContext);
+  if (context === undefined) {
+    throw new Error('useNovelData must be used within a ConvertDataProvider');
+  }
+  return context;
+};
+
+const useCharaterData = () => {
+  const context = useContext(CharacterContext);
+  if (context === undefined) {
+    throw new Error(
+      'useCharaterData must be used within a ConvertDataProvider'
+    );
+  }
+  return context;
+};
+
+export {
+  ConvertDataProvider,
+  useNovelTitleData,
+  useNovelData,
+  useCharaterData,
+};
