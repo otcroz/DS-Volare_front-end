3a4215784bcef9808fa2112269d639a17f2265dc
otcroz <dbtndus0527@gmail.com>
2024-08-30 05:17:03 +0900

    [feat] Implement the stack bar chart in statistics level
    Guthub issue #54

--- src/component/convert/statistics/StatisticsBox.tsx
+++ src/component/convert/statistics/StatisticsBox.tsx
@@ -8,6 +8,12 @@ import {
 import { motion } from 'framer-motion';
 import { useAnimationContext } from '../../../context/animationContext';
 import Mindmap from './Mindmap';
+import { useQuery } from '@tanstack/react-query';
+import { queryKeys } from '../../../utils/queryKeys';
+import { useConvert } from '../../../hooks/useConvert';
+import Spinner from '../../base/Spinner';
+import { spinnerText } from '../../../utils/spinnerText';
+import RateChart from './RateChart';
 
 type props = {
   data: string;
@@ -15,12 +21,26 @@ type props = {
 
 const StatisticsBox = forwardRef<HTMLDivElement, props>(({ data }, ref) => {
   const { controlStatistics } = useAnimationContext();
+  const { apperanceRate } = useConvert();
+
+  const appearanceQuery = useQuery({
+    queryKey: queryKeys.appearance,
+    queryFn: () => apperanceRate(31),
+  });
+
   return (
-    <motion.div ref={ref} animate={controlStatistics} style={{ opacity: 0 }}>
+    <motion.div ref={ref} animate={controlStatistics} style={{ opacity: 100 }}>
       <GlassBox hasData={true}>
         <TitleText>통계</TitleText>
-        <ContentBox>
-          <Mindmap />
+        <ContentBox style={{ overflowY: 'scroll' }}>
+          {!appearanceQuery.isLoading ? (
+            <>
+              <Mindmap />
+              <RateChart result={appearanceQuery.data.result} />
+            </>
+          ) : (
+            <Spinner text={spinnerText.statistics} />
+          )}
         </ContentBox>
       </GlassBox>
     </motion.div>
--- src/utils/spinnerText.ts
+++ src/utils/spinnerText.ts
@@ -2,4 +2,5 @@ export const spinnerText = {
   character: '등장인물을 인식하는 중...',
   scripts: '소설을 대본으로 변환하는 중...',
   storyboard: '대본을 기반으로 스토리보드를 만드는 중...',
+  statistics: '작품의 통계 정보를 가져오는 중...',
 };
